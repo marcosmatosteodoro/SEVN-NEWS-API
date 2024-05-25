6c32dcd37184d3f20e85332581709f0886506fb2
Marcos Paulo <mp.matos25@gmail.com>
2024-05-25 12:13:20 -0300

    feat: add cors and use json

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
+import cors from 'cors';
 import express from "express";
 import router from "./router";
-
 class App {
   public server: express.Application
 
   constructor() {
     this.server = express()
+    this.middleware()
     this.router()
+  }
+
+  private middleware(){
+    this.server.use(express.json());
+    this.server.use(cors());
   }
 
   private router(){
