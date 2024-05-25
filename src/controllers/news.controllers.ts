537f8d9897b1567ff9542a148df29b7be505f918
Marcos Paulo <mp.matos25@gmail.com>
2024-05-25 12:26:40 -0300

    feat: create all endpoints

--- src/controllers/news.controllers.ts
+++ src/controllers/news.controllers.ts
@@ -8,10 +8,29 @@ class NewsController {
   constructor() {
     this.data = rawData as News[];
     this.index = this.index.bind(this);
+    this.show = this.show.bind(this);
+    this.headline = this.headline.bind(this);
+    this.secondary = this.secondary.bind(this);
   }
 
 
   public index (req: Request, res: Response) {
+    res.json(
+      this.data
+    );
+  }
+
+  public show (req: Request, res: Response) {
+    res.json(
+      this.data
+    );
+  }
+  public headline (req: Request, res: Response) {
+    res.json(
+      this.data
+    );
+  }
+  public secondary (req: Request, res: Response) {
     res.json(
       this.data
     );
--- src/router.ts
+++ src/router.ts
@@ -3,8 +3,9 @@ import { newsController } from "./controllers/news.controllers";
 
 const router: Router = Router();
 
-
-router.get("/news", newsController.index);
-
+// router.get("/news", newsController.index);
+router.get("/news/headline", newsController.headline);
+router.get("/news/secondary", newsController.secondary);
+router.get("/news/:id", newsController.show);
 
 export default router;
