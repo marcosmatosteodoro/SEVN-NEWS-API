6f00db8452f0ca97677b11082f647ac426cae06f
Marcos Paulo <mp.matos25@gmail.com>
2024-05-27 08:12:25 -0300

    feat: change api enpoints

--- src/router.ts
+++ src/router.ts
@@ -4,8 +4,8 @@ import { newsController } from "./controllers";
 const router: Router = Router();
 
 // router.get("/news", newsController.index);
-router.get("/news/headline", newsController.headline);
-router.get("/news/secondary", newsController.secondary);
-router.get("/news/:id", newsController.show);
+router.get("/api/news/headline", newsController.headline);
+router.get("/api/news/secondary", newsController.secondary);
+router.get("/api/news/:id", newsController.show);
 
 export default router;
